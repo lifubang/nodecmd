a7e35eeba8b3c21607c37c07c4c756627be28eea
86ab559424c94d418c70fd272f96fe9a <lifubang@ciic.com.cn>
2017-08-23 19:48:42 +0800

    add gets, printsth, readInt, and readDouble api.

--- test/test.js
+++ test/test.js
@@ -4,17 +4,26 @@
 var cmd = require('../index');
 
 function test() {
-    cmd.print('This output is from nodecmd interface:\n');
-    cmd.print(1234567890.123);
-    cmd.print('\n');
-    cmd.print('Hello workd.');
-    cmd.print('\n');
-    cmd.print('Please input something:\n');
+    cmd.printsth('This output is from nodecmd interface:\n');
+    cmd.printsth(1234567890.123);
+    cmd.printsth('\n');
+    cmd.printsth('Hello workd.');
+    cmd.printsth('\n');
 
+    cmd.print('Please input 10 characters:');
+    cmd.print(cmd.gets(10));
+
+    cmd.printsth('Please input something:\n');
     var input = cmd.read_line();
-    cmd.print('Your input is:\n');
-    cmd.print(input);
-    cmd.print('\n');
+    cmd.printsth('Your input is:\n');
+    cmd.printsth(input);
+    cmd.printsth('\n');
+
+    cmd.print('Please input a int:');
+    cmd.print(cmd.readInt());
+
+    cmd.print('Please input a double:');
+    cmd.print(cmd.readDouble());
 }
 
 module.exports = test;
